79b485b5eca7f8f5b37bf8f80a3ab0f781ea6309
huy041203 <huy041203@gmail.com>
2024-10-17 07:16:58 +0700

    setup Firebase Auth , Cloud Firestore, Realtime DB

--- app/src/main/java/com/midterm/appchatt/MainActivity.java
+++ app/src/main/java/com/midterm/appchatt/MainActivity.java
@@ -1,24 +1,110 @@
 package com.midterm.appchatt;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class MainActivity extends AppCompatActivity {
 
+    private  DatabaseReference myRef;
+    private FirebaseDatabase database;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("message");
+        readDataFromRealtimeDB();
+        postDataToRealtimeDB("Hello World");
+        login("huy041203@gmail.com","hu");
+        createNewUser("newUser@gmail.com","newuser");
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+    private void login(String email, String pass){
+        mAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Log.d("DEBUG", "signInWithEmail:success");
+                }
+                else {
+                    Log.d("DEBUG", "signInWithEmail:failure");
+                }
+            }
+        });
+    }
+    private void createNewUser(String newUserEmail,String newUserPassword){
+        mAuth.createUserWithEmailAndPassword(newUserEmail,newUserPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Log.d("DEBUG", "success");
+                }
+                else {
+                    Log.d("DEBUG", "task failed successfully");
+                }
+            }
+        });
+    }
+    private void postDataToRealtimeDB(String data){
+
+        myRef.setValue(data).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Log.d("DEBUG", "create new data" +data +" successfully");
+                }
+                else {
+                    Log.d("DEBUG", "fail to create data");
+                }
+            }
+        });
+
+    }
+    private void readDataFromRealtimeDB(){
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String value = snapshot.getValue(String.class);
+                Log.d("DEBUG", "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.w("DEBUG", "Failed to read value.", error.toException());
+            }
+        });
+    }
 }
+
+
