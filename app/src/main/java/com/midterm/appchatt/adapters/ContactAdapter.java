47ddeb4ed34b8898bc2e6e62c609fb8fcf1d6379
antialberteinstein <antialberteinstein@gmail.com>
2024-10-20 13:36:40 +0700

    Fix recyclerview's toplimit (scrolling to the void)

--- app/src/main/java/com/midterm/appchatt/adapters/ContactAdapter.java
+++ app/src/main/java/com/midterm/appchatt/adapters/ContactAdapter.java
@@ -1,10 +1,12 @@
 package com.midterm.appchatt.adapters;
 
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.midterm.appchatt.R;
 import com.midterm.appchatt.databinding.ContactItemBinding;
 import com.midterm.appchatt.models.Contact;
 
@@ -31,6 +33,9 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ChatView
         if (position + 1 == this.getItemCount()) {
             holder.hideSeparator();
         }
+        if (position == 0) {
+            holder.showMarginTopForFirstItem();
+        }
     }
 
     @Override
@@ -52,6 +57,13 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ChatView
                 // If there is an url of avatar...
             }
             binding.messageDisplayName.setText(contact.getDisplayName());
+        }
+
+        // Show margin top for the first item instead of entire recycler view for
+        //        avoiding the bug that some items unexpectedly hide (bad effect)
+        //        into the void.
+        public void showMarginTopForFirstItem() {
+            this.binding.firstElementMargin.setVisibility(View.VISIBLE);
         }
 
         public void hideSeparator() {
